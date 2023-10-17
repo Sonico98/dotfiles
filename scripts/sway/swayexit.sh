1335944b15b3163c540150ced6457a61403a9132
Sonico98 <lol6028@gmail.com>
2023-10-17 01:33:18 -0300

    Adapt exit script to sway. Leave systemd user services running for now

---
+++ scripts/sway/swayexit.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sysclose () {
+	sync_cache_to_hdd
+	# stop_user_services
+	kill -TERM "$(pidof firefox)"
+	kill -TERM "$(pidof qbittorrent)"
+	kill "$(pidof telegram-desktop)"
+	kill "$(pidof clight)"
+	kill "$(pidof wl-mpris-idle-inhibit)"
+	swaymsg [app_id=".*"] kill
+	swaymsg [class=".*"] kill
+	kill -9 "$(pgrep -f "python")"
+}
+
+
+sync_cache_to_hdd () {
+	rsync -a --update --existing ~/.cache/paru/clone/ ~/.local/share/little-cache-files/paru/clone/
+}
+
+
+stop_user_services () {
+	systemctl --user stop pipewire-input-filter-chain.service \
+	pipewire-media-session.service pipewire-pulse.service \
+	pipewire-pulse.socket pipewire.service pipewire.socket \
+	wireplumber.service mpdscribble.service \
+	mpDris2.service mpd.service playerctld.service \
+	rangerbot.service userbot.service darkman.service \
+	ssh-agent.service xdg-desktop-portal.service
+}
+
+
+case "$1" in
+    exit)
+	sysclose
+	sleep 1 && swaymsg exit
+	;;
+
+    reboot)
+	sysclose
+	sleep 1 && systemctl reboot &
+	;;
+
+    shutdown)
+	sysclose
+	sleep 1 && systemctl poweroff -i &
+	;;
+esac
