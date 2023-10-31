4606085004d90c9f5be1b08752c73d02b0c2dcd8
Sonico98 <lol6028@gmail.com>
2023-10-31 17:01:22 -0300

    Set prefer-dark-theme to false for light mode

--- local/share/light-mode.d/10_gtk.sh
+++ local/share/light-mode.d/10_gtk.sh
@@ -10,11 +10,11 @@ sed -i -e "s#gtk-theme-name=.*#gtk-theme-name=\"$light_gtk_theme\"#" \
 # GTK 3
 sed -i -e "s#gtk-theme-name=.*#gtk-theme-name=\"$light_gtk_theme\"#" \
 	-e "s#gtk-icon-theme-name=.*#gtk-icon-theme-name=\"$light_gtk_icon\"#" \
-	-e "s#gtk-application-prefer-dark-theme=.*#gtk-application-prefer-dark-theme=true#" \
+	-e "s#gtk-application-prefer-dark-theme=.*#gtk-application-prefer-dark-theme=false#" \
 	"$HOME/.config/gtk-3.0/settings.ini"
 # GTK 4
 sed -i -e "s#gtk-theme-name=.*#gtk-theme-name=\"$light_gtk_theme\"#" \
 	-e "s#gtk-icon-theme-name=.*#gtk-icon-theme-name=\"$light_gtk_icon\"#" \
-	-e "s#gtk-application-prefer-dark-theme=.*#gtk-application-prefer-dark-theme=true#" \
+	-e "s#gtk-application-prefer-dark-theme=.*#gtk-application-prefer-dark-theme=false#" \
 	"$HOME/.config/gtk-4.0/settings.ini"
 
