16f2c12fff19b37c07464e26f02f006f98cc7f14
Sonico98 <lol6028@gmail.com>
2023-11-03 16:41:43 -0300

    Change wallpaper

--- local/share/darkman-common.d/wallpaper.sh
+++ local/share/darkman-common.d/wallpaper.sh
@@ -2,4 +2,4 @@
 
 light_wallpaper="/mnt/Heaven/Pictures/Anime/The iDOLM@STER/[0] Million Live/Theater Days/Desktop Wallpapers/Momoko Suou/1698748827519575.jpg"
 
-dark_wallpaper="/mnt/Heaven/Pictures/Anime/Date a Live/Kurumi Tokisaki/fa61dc2ef271065fd17c887644e04326.jpg"
+dark_wallpaper="/home/sonico/Pictures/Anime/The iDOLM@STER/[0] Million Live/Theater Days/Desktop Wallpapers/Momoko Suou/Momoko in Wonderland.png"
