c02727b3653179b01513240a9377a4bf2e24f38b
Sonico98 <lol6028@gmail.com>
2023-11-07 15:28:23 -0300

    Pause brightness calibration when locking, and resume afterwards

--- scripts/lockscreen/swaylockbg.sh
+++ scripts/lockscreen/swaylockbg.sh
@@ -10,12 +10,15 @@ if [[ ! -d ${LOCKSCREEN_DIR} ]]; then
 	convert ${LOCKSCREEN_DIR}/lock.png -scale 10% -blur 0x1.5 -resize 1000% ${LOCKSCREEN_DIR}/lockscreen.jpg
 fi
 
-# Pause music, mute audio and microphone
+# Pause music and brightness auto-calibration, mute audio and microphone.
 playerctl --player=%any,chromium pause
 wpctl set-mute @DEFAULT_AUDIO_SINK@ 1
 wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 1
+busctl --expect-reply=false --user set-property org.clight.clight /org/clight/clight/Conf/Backlight org.clight.clight.Conf.Backlight NoAutoCalib "b" false
 swaymsg "output * power off"
 swaylock -F --indicator-idle-visible -i ${LOCKSCREEN_DIR}/lockscreen.jpg
+# Undo the previous actions after the screen has been unlocked
 wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
 wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 0
+busctl --expect-reply=false --user set-property org.clight.clight /org/clight/clight/Conf/Backlight org.clight.clight.Conf.Backlight NoAutoCalib "b" true
 rm -rf ${LOCKSCREEN_DIR}
