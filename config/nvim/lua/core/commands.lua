874555967893d4a92bf45073d005b1afd042b199
Sonico98 <lol6028@gmail.com>
2024-01-02 15:42:50 -0300

    Add command to copy all search matches

--- config/nvim/lua/core/commands.lua
+++ config/nvim/lua/core/commands.lua
@@ -6,3 +6,14 @@ vim.api.nvim_create_autocmd(
 	"BufReadPost" ,
 	{ command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
 )
+
+-- Copy all text that match a search
+vim.cmd([[
+	function! CopyMatches(reg)
+	let hits = []
+	%s//\=len(add(hits, submatch(0))) ? submatch(0) : ''/gne
+	  let reg = empty(a:reg) ? '+' : a:reg
+	  execute 'let @'.reg.' = join(hits, "\n") . "\n"'
+	endfunction
+	command! -register CopyMatches call CopyMatches(<q-reg>)
+]])
