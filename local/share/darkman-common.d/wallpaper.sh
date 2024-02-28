5efc82bea89e023a0b6edd2b8f1d070adb797dac
Sonico98 <lol6028@gmail.com>
2024-02-28 15:39:04 -0300

    Change wallpaper location again

--- local/share/darkman-common.d/wallpaper.sh
+++ local/share/darkman-common.d/wallpaper.sh
@@ -3,5 +3,5 @@
 light_wallpaper="/mnt/Heaven/Pictures/Anime/The iDOLM@STER/[0] Million Live/Theater Days/Desktop Wallpapers/Momoko Suou/1698748827519575.jpg"
 # light_wallpaper="/home/sonico/Pictures/Illustrations/Desktop Wallpapers/1094777.jpg"
 
-dark_wallpaper="/home/sonico/Pictures/Other/Unsorted/Twitter/OW4vpalfz/(Twitter) [OW4vpalfz] 1747238072360845554 - p0.jpg"
+dark_wallpaper="/mnt/Heaven/Pictures/Other/Unsorted/Twitter/OW4vpalfz/Desktop Wallpapers/(Twitter) [OW4vpalfz] 1747238072360845554 - p0.jpg"
 # dark_wallpaper="/home/sonico/Pictures/Illustrations/Desktop Wallpapers/1094777.jpg"
