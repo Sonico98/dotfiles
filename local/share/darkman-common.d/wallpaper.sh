0d77c44509d15480d9e7bffde4c496002252bf23
Sonico98 <sonico+gh@disroot.org>
2025-03-10 17:03:45 -0300

    Misc visual or config updates

--- local/share/darkman-common.d/wallpaper.sh
+++ local/share/darkman-common.d/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # light_wallpaper="/mnt/Heaven/Pictures/Anime/The iDOLM@STER/[0] Million Live/Theater Days/Desktop Wallpapers/Momoko Suou/1698748827519575.jpg"
-light_wallpaper="/usr/share/wallpapers/ScarletTree/contents/images/5120x2880.png"
+light_wallpaper="/mnt/Heaven/Pictures/Anime/Hololive/Per-girl Pictures/Houshou Marine/MVs/[MTBB] Unison (BD 1080p) [A702DD8B]24-00:03:27.png"
 
 dark_wallpaper="/mnt/Heaven/Pictures/Anime/Hololive/Per-girl Pictures/Houshou Marine/MVs/[MTBB] Unison (BD 1080p) [A702DD8B]21-00:01:53.png"
 # dark_wallpaper="/home/sonico/Pictures/Anime/Unsorted/Artists/(0) Others/Desktop Wallpapers/media_FTHzurCVEAAEpx7_waifu2x_art_scan_noise0_scale.png"
