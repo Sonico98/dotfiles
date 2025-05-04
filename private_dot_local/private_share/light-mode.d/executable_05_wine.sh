dbfb9f58d02ba3ee52d6943ff61191c712d0b258
Sonico98 <sonico+gh@disroot.org>
2025-05-04 20:46:41 -0300

    Make wine themeing non-blocking (hopefully, not fully tested)

--- private_dot_local/private_share/light-mode.d/executable_05_wine.sh
+++ private_dot_local/private_share/light-mode.d/executable_05_wine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-WINEPREFIX=/home/sonico/.wine wine regedit /home/sonico/.config/wine/wine-reset-theme.reg
+WINEPREFIX=/home/sonico/.wine nohup wine regedit /home/sonico/.config/wine/wine-reset-theme.reg
