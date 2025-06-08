d447ea3d703956354d212a8bf4b501e4f20d6676
Sonico98 <sonico+gh@disroot.org>
2025-06-08 19:23:40 -0300

    Misc

--- private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
+++ private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
@@ -3,6 +3,6 @@
 # light_wallpaper="/mnt/Heaven/Pictures/Anime/The iDOLM@STER/[0] Million Live/Theater Days/Desktop Wallpapers/Momoko Suou/1698748827519575.jpg"
 light_wallpaper="/mnt/Heaven/Pictures/Anime/Unsorted/Desktop Wallpapers/(Pixiv) [桐月レイカ] 紫雲清夏 - 128311997_p0.png"
 
-dark_wallpaper="/mnt/Heaven/Pictures/Anime/Unsorted/Artists/ashenmash/Desktop Wallpapers/(Pixiv) [ashenmash] マシュ・キリエライト_118224389 p1.png"
+dark_wallpaper="/home/sonico/Pictures/Anime/Unsorted/Artists/yotsuba_haru/Desktop Wallpapers/(Twitter) [yotsuba_haru] 1762772236929585464 - p0.jpg"
 # dark_wallpaper="/home/sonico/Pictures/Anime/Unsorted/Artists/(0) Others/Desktop Wallpapers/media_FTHzurCVEAAEpx7_waifu2x_art_scan_noise0_scale.png"
 # dark_wallpaper="/usr/share/wallpapers/Next/contents/images_dark/1920x1080.png"
