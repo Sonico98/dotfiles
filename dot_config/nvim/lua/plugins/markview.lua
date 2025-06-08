514e258bfa191cf133881f5862b7286b7b26754d
Sonico98 <sonico+gh@disroot.org>
2025-06-08 20:14:58 -0300

    Update markview plugin config

--- dot_config/nvim/lua/plugins/markview.lua
+++ dot_config/nvim/lua/plugins/markview.lua
@@ -7,18 +7,17 @@ return {
 	},
 	config = function ()
 		require("markview").setup({
-			modes = { "n", "no", "c", "i" }, -- Change these modes
 			-- to what you need
 
-			hybrid_modes = { "i" },     -- Uses this feature on
-			-- normal mode
-
-			-- This is nice to have
-			callbacks = {
-				on_enable = function (_, win)
-					vim.wo[win].conceallevel = 2;
-					vim.wo[win].concealcursor = "c";
-				end
+			preview = {
+				modes = { "n", "no", "c", "i" }, -- Change these modes
+				hybrid_modes = { "i" },     -- Uses this feature on
+				callbacks = {
+					on_enable = function (_, win)
+						vim.wo[win].conceallevel = 2;
+						vim.wo[win].concealcursor = "c";
+					end
+				}
 			}
 		})
 	end
