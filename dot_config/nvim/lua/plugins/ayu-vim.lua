c932281cc0c989bc6eecf73f7237aba10ee8e65d
Sonico98 <sonico+gh@disroot.org>
2025-06-13 01:06:16 -0300

    More readable neovim comments with transparent terminal

--- dot_config/nvim/lua/plugins/ayu-vim.lua
+++ dot_config/nvim/lua/plugins/ayu-vim.lua
@@ -8,12 +8,20 @@ return {
 		vim.g.ayu_extended_palette = 1
 		-- Set theme overrides
 		local function custom_ayu_colors()
+
+			-- Add two extra lines to ~/.local/share/nvim/lazy/ayu-vim/autoload/ayu.vim
+			-- One under if s:extended_palette
+			-- let g:ayu#palette['extended_comment']   = {'light': '#adafb2', 'mirage': '#6e7c8e', 'dark': '#858585'}
+			-- The second after the following else
+			-- let g:ayu#palette['extended_comment']   = g:ayu#palette['syntax_comment']
+
 			local overrides = {
 				{ "Normal", "", "" },
 				{ "SignColumn", "", "" },
 				{ "LineNr", "ui_fg", "" },
 				{ "CursorLine", "", "editor_line" },
 				{ "CursorLineNR", "common_accent", "editor_line" },
+				{ "Comment", "extended_comment", "", vim.g.ayu_italic_comment and 'italic' or '' },
 			}
 
 			for _, colors in ipairs(overrides) do
