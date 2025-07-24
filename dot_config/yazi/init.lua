2372c83b2d75a07cf1917d5bd18aa56fefa2ef4d
Sonico98 <sonico+gh@disroot.org>
2025-07-24 17:56:05 -0300

    Add and configure project management for yazi

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -38,3 +38,51 @@ require("session"):setup {
 require("mime-preview"):setup()
 
 require("git"):setup()
+
+require("projects"):setup({
+    save = {
+        method = "yazi", -- yazi | lua
+        yazi_load_event = "@projects-load", -- event name when loading projects in `yazi` method
+        lua_save_path = "", -- path of saved file in `lua` method, comment out or assign explicitly
+                            -- default value:
+                            -- windows: "%APPDATA%/yazi/state/projects.json"
+                            -- unix: "~/.local/state/yazi/projects.json"
+    },
+    last = {
+        update_after_save = true,
+        update_after_load = true,
+        load_after_start = false,
+    },
+    merge = {
+        event = "projects-merge",
+        quit_after_merge = false,
+    },
+    event = {
+        save = {
+            enable = true,
+            name = "project-saved",
+        },
+        load = {
+            enable = true,
+            name = "project-loaded",
+        },
+        delete = {
+            enable = true,
+            name = "project-deleted",
+        },
+        delete_all = {
+            enable = true,
+            name = "project-deleted-all",
+        },
+        merge = {
+            enable = true,
+            name = "project-merged",
+        },
+    },
+    notify = {
+        enable = true,
+        title = "Projects",
+        timeout = 3,
+        level = "info",
+    },
+})
