78ed7f2bf7ec794c4abe13ffce8fa2c59b9c967d
Sonico98 <sonico+gh@disroot.org>
2025-09-05 03:12:47 -0300

    [nvim] Change chezmoi plugin
    This one enables syntax highlighting on files and executes templates

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -1,22 +1,15 @@
 return {
-  'xvzc/chezmoi.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  init = function()
-		--  e.g. ~/.local/share/chezmoi/*
-	vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-		pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
-		callback = function(ev)
-			local bufnr = ev.buf
-			local edit_watch = function()
-				require("chezmoi.commands.__edit").watch(bufnr)
-			end
-			vim.schedule(edit_watch)
-		end,
-	})
-  end,
-  config = function()
-    require("chezmoi").setup {
-      -- your configurations
-    }
-  end
+	"andre-kotake/nvim-chezmoi",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ "nvim-telescope/telescope.nvim" },
+	},
+	opts = {
+		edit = {
+			apply_on_save = "auto",
+		},
+	},
+	config = function(_, opts)
+		require("nvim-chezmoi").setup(opts)
+	end,
 }
