44585140166f7767452a7e77a4c59e9229633d95
Sonico98 <sonico+gh@disroot.org>
2025-09-17 23:46:28 -0300

    [darkman] Allow setting a different wallpaper per output

--- private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
+++ private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# light_wallpaper="/mnt/Heaven/Pictures/Anime/The iDOLM@STER/[0] Million Live/Theater Days/Desktop Wallpapers/Momoko Suou/1698748827519575.jpg"
+# Syntax:
+# - A different wallpaper per output: OUTPUT·WALLPAPER¤OUTPUT·WALLPAPER
+# - The same wallpaper across all outputs: <WALLPAPER>
+#
+# You can get output names executing `swaymsg -t get_outputs` or `wlr-randr`
 light_wallpaper="/mnt/Heaven/Pictures/Anime/Unsorted/Desktop Wallpapers/(Pixiv) [桐月レイカ] 紫雲清夏 - 128311997_p0.png"
 
-dark_wallpaper="/home/sonico/Downloads/Firefox Downloads/Images/Twitter/(Twitter) [NzeNeee] 1893275751673135331 - p0.jpg"
-# dark_wallpaper="/home/sonico/Pictures/Anime/Unsorted/Artists/(0) Others/Desktop Wallpapers/media_FTHzurCVEAAEpx7_waifu2x_art_scan_noise0_scale.png"
-# dark_wallpaper="/usr/share/wallpapers/Next/contents/images_dark/1920x1080.png"
+dark_wallpaper="VGA-1·/home/sonico/Downloads/Firefox Downloads/Images/Twitter/(Twitter) [NzeNeee] 1893275751673135331 - p0.jpg¤LVDS-1·/home/sonico/Downloads/Firefox Downloads/Images/Pixiv/(Pixiv) [chelsea] Supernova - 128882554_p0.png"
