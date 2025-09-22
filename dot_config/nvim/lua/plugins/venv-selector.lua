2b46676ad4b885bb22312f105a29f026f8a8d7a0
Sonico98 <sonico+gh@disroot.org>
2025-09-21 23:38:02 -0300

    Misc - Mimeapps, nvim plugins, yazi plugins, wallpaper

--- dot_config/nvim/lua/plugins/venv-selector.lua
+++ dot_config/nvim/lua/plugins/venv-selector.lua
@@ -6,7 +6,6 @@ return {
 		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
 	},
 	lazy = false,
-	branch = "regexp", -- This is the regexp branch, use this for the new version
 	keys = {
 		{ "<leader>v", "<cmd>VenvSelect<cr>" },
 	},
--- private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
+++ private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
@@ -5,6 +5,6 @@
 # - The same wallpaper across all outputs: <WALLPAPER>
 #
 # You can get output names executing `swaymsg -t get_outputs` or `wlr-randr`
-light_wallpaper="/mnt/Heaven/Pictures/Anime/Unsorted/Desktop Wallpapers/(Pixiv) [桐月レイカ] 紫雲清夏 - 128311997_p0.png"
+light_wallpaper="VGA-1·/mnt/Heaven/Pictures/Anime/Unsorted/Desktop Wallpapers/(Pixiv) [桐月レイカ] 紫雲清夏 - 128311997_p0.png¤LVDS-1·/mnt/Heaven/Downloads/Images/Pixiv/Desktop Wallpapers/(Pixiv) [Huhv.] Sea of Flowers - 129544598_p0.jpg"
 
-dark_wallpaper="VGA-1·/home/sonico/Downloads/Firefox Downloads/Images/Twitter/(Twitter) [NzeNeee] 1893275751673135331 - p0.jpg¤LVDS-1·/home/sonico/Downloads/Firefox Downloads/Images/Pixiv/(Pixiv) [chelsea] Supernova - 128882554_p0.png"
+dark_wallpaper="VGA-1·/home/sonico/Downloads/Images/Twitter/(Twitter) [NzeNeee] 1893275751673135331 - p0.jpg¤LVDS-1·/home/sonico/Downloads/Images/Pixiv/Desktop Wallpapers/(Pixiv) [chelsea] Supernova - 128882554_p0.png"
