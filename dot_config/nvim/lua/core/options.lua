04d7bbb3469f60f21cfdd20dd80225d956d7ee68
Sonico98 <sonico+gh@disroot.org>
2025-09-22 00:13:05 -0300

    [nvim] Allow copying to the clipboard over SSH

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -41,3 +41,25 @@ local disabled_built_ins = {
 	"bugreport",
 	"tutor"
 }
+
+-- Allow copying to clipboard over SSH
+local function paste()
+  return {
+    vim.split(vim.fn.getreg(''), '\n'),
+    vim.fn.getregtype(''),
+  }
+end
+
+if vim.env.SSH_TTY then
+	vim.g.clipboard = {
+		name = 'OSC 52',
+		copy = {
+			['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+			['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+		},
+		paste = {
+			['+'] = paste,
+			['*'] = paste,
+		},
+	}
+end
