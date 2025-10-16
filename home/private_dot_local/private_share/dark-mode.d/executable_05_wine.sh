6534080ae955e1e94433c90b5382679800d21dbc
Sonico98 <sonico+gh@disroot.org>
2025-10-15 22:42:35 -0300

    [darkman] Hopefully make wine async

--- home/private_dot_local/private_share/dark-mode.d/executable_05_wine.sh
+++ home/private_dot_local/private_share/dark-mode.d/executable_05_wine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-WINEPREFIX=/home/sonico/.wine nohup wine regedit /home/sonico/.config/wine/wine-breeze-dark.reg
+WINEPREFIX=/home/sonico/.wine nohup wine regedit /home/sonico/.config/wine/wine-breeze-dark.reg &
