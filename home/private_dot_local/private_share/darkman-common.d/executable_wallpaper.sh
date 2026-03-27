72dfc6c91776d38150f19f4607cef18817f81c59
Sonico98 <sonico+gh@disroot.org>
2026-03-26 23:28:25 -0300

    [swww] Reflect upstream rename to awww

--- home/private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
+++ home/private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
@@ -7,13 +7,13 @@
 # You can get output names executing `swaymsg -t get_outputs` or `wlr-randr`
 
 # Light Wallpaper
-VGA="/home/sonico/Pictures/Wallpapers/(Pixiv) [Hiroko] 栞 - 128509050_p0.jpg"
-LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [niyemiiii] 无题 - 104627556_p0_modified.jpg"
+LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [Hiroko] 栞 - 128509050_p0.jpg"
+# LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [niyemiiii] 无题 - 104627556_p0_modified.jpg"
 # LVDS="/usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png"
 light_wallpaper="VGA-1·$VGA¤LVDS-1·$LVDS"
 
 # Dark Wallpaper
-VGA="/home/sonico/Pictures/Wallpapers/(Pixiv) [海奏hzh] 狼嵜光 - 127970978_p0.png"
-LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [れるて] 初風 - 136209794_p0.jpg"
+LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [海奏hzh] 狼嵜光 - 127970978_p0.png"
+# LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [れるて] 初風 - 136209794_p0.jpg"
 # LVDS="/usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png"
 dark_wallpaper="VGA-1·$VGA¤LVDS-1·$LVDS"
