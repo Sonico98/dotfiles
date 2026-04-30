e944cc9e30f69976418b50fb14aff3731c63581a
Sonico98 <sonico+gh@disroot.org>
2026-04-30 12:40:12 -0300

    Misc - Update some deps, disable rounded corners for now, change wp, swayimg changed its config format again... (broken)

--- home/private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
+++ home/private_dot_local/private_share/darkman-common.d/executable_wallpaper.sh
@@ -8,12 +8,12 @@
 
 # Light Wallpaper
 LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [Hiroko] 栞 - 128509050_p0.jpg"
-# LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [niyemiiii] 无题 - 104627556_p0_modified.jpg"
+VGA="/home/sonico/Pictures/Wallpapers/(Pixiv) [niyemiiii] 无题 - 104627556_p0_modified.jpg"
 # LVDS="/usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png"
 light_wallpaper="VGA-1·$VGA¤LVDS-1·$LVDS"
 
 # Dark Wallpaper
 LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [海奏hzh] 狼嵜光 - 127970978_p0.png"
-# LVDS="/home/sonico/Pictures/Wallpapers/(Pixiv) [れるて] 初風 - 136209794_p0.jpg"
+VGA="/home/sonico/Pictures/Wallpapers/(Pixiv) [れるて] 初風 - 136209794_p0.jpg"
 # LVDS="/usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png"
 dark_wallpaper="VGA-1·$VGA¤LVDS-1·$LVDS"
